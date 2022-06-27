e99560bcb153466c13e0c203407bd3a63a86c642
Renê Alves <renepvh11@gmail.com>
2022-06-27 15:42:31 -0400

    req 50

--- src/Components/IngredientsProgress.jsx
+++ src/Components/IngredientsProgress.jsx
@@ -1,9 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useParams, useHistory } from 'react-router-dom';
 import styles from '../Css/IngredientChecked.module.css';
 
 function IngredientsProgress({ data }) {
+  const { id } = useParams();
+  const history = useHistory();
+  const { pathname } = history.location;
+  const type = pathname.includes('foods') ? 'meals' : 'cocktails';
+
   const [ingredientChecked, setIngredientChecked] = useState([]);
+
+  useEffect(() => {
+    const local = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    // console.log(local);
+    if (local !== null && (local[type]) !== undefined && local[type][id] !== undefined) {
+      setIngredientChecked(local[type][id]);
+    }
+    // (local[type][id] ? setIngredientChecked(local[type][id]) : '');
+
+    // setIngredientChecked(local[type][id]);
+    // console.log(type, id);
+  }, []);
+
+  const localChange = (name) => {
+    console.log(name);
+    console.log(data);
+    console.log(id);
+    console.log(pathname);
+    console.log(type);
+    // console.log(localStg);
+    // const localStg = JSON.parse(localStorage
+    //   .getItem('inProgressRecipes'))[type][id] || [];
+    // const names = [name];
+    const newName = {
+      [type]: { [id]: [...ingredientChecked, name] } };
+
+    return localStorage.setItem('inProgressRecipes', JSON.stringify(newName));
+
+    // if (!local[type][id]) {
+    //   setInProgressItems([name]);
+    //   return localStorage.setItem('inProgressRecipes', JSON.stringify(newName));
+    // }
+  };
 
   const saveIngredients = (name) => {
     if (ingredientChecked.includes(name)) {
@@ -36,6 +75,8 @@ function IngredientsProgress({ data }) {
             <input
               type="checkbox"
               id={ ingredients[index] }
+              checked={ ingredientChecked.includes(ingredients[index]) }
+              onChange={ () => localChange(ingredients[index]) }
               onClick={ () => saveIngredients(ingredients[index]) }
             />
             {`${ingredients[index]} - ${measure}`}
